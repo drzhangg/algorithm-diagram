826d58211bc62ce556b566ed5f6f4b741eb28302
zhang <654014730@qq.com>
2021-02-15 19:47:01 +0800

    add

---
+++ array/广度优先搜索.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+graph = {}
+
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller!")
+
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+
+
+def person_is_seller(name):
+    return name[-1] == 'm'
